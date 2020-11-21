79034a7f11ca12b8a02d364c687cda644d8a3067
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-11-20 21:49:49 -0500

    a number generate is added to the quest objet

--- src/Components/Quest.js
+++ src/Components/Quest.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Quest = () => {
 
+    const rgbColor = {};
+
+    const color = () => {
+        const colorNum = 255;
+        rgbColor.r = Math.floor(Math.random() * colorNum);
+        rgbColor.g = Math.floor(Math.random() * colorNum);
+        rgbColor.b = Math.floor(Math.random() * colorNum);
+    }
+    color();
+    console.log(rgbColor)
+   
+    
+
     return(
-        <span>rgb(#,#,#)</span>
+        <span>rgb( {rgbColor.r},{rgbColor.g},{rgbColor.b} )</span>
     )
 };
 
